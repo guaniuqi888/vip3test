9b74d2e1265983975fca89b6132b125bcbc817ae
guaniuqi888 <524873987@qq.com>
2019-09-21 19:11:53 +0800

    初始化仓库

---
+++ test.py
@@ -0,0 +1,98 @@
+'''
+a=5
+b=2.5
+c='hello'
+d="hello word"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+'''
+#a,b=input('please input a value:').split(',')#同时输入多个值用split
+#print('the vaule of is:',a,type(a),type(b))
+#print('xxx={},yyy={}'.format(a,b))
+
+#a=5
+#b=2.5
+#print("%d,%f"%(a,b))
+
+#a,b=input('please input a value:').split(',') #同时输入多个值用split
+#print('the vaule of is:',a,type(a),type(b))
+#print('xxx={},yyy={}'.format(a,b))
+#print("%s,%d"%(a,int(b)))
+'''
+a=[1,2,3,4,5,6,468,8,9]
+print(a[-3])
+'''
+'''
+a=[1,2,3,4,5,6,468,8,9]
+print(list(range(0,4)))
+print(list(range(1,4)))
+'''
+s=[10,1,2,3,7,4,5,6,8,9]
+#s.reverse()
+#s.sort()
+#print(sorted(s))
+# s.insert(3,303)
+# s.insert(6,666)
+# s.append(8)
+# print(max(s))
+# print(min(s))
+# print(len(s))
+# del s[3]
+# print(s)
+# print(s.index(10))
+s.clear()
+# print(s)
+# s.append(6)
+# print(s)
+'''
+s1=[1,2,3]
+s2=[4,5,6]
+s1.extend(s2)
+print(s1)
+'''
+'''
+s=[11,13,5,7,0,56,23,34,72]
+print(max(s))
+print(min(s))
+print(len(s))
+print(s.index(5))
+s.append(7)
+print(s)
+del s[4]
+print(s)
+s.sort()#按照从小到大排序
+s.reverse()#只能倒序不能排序
+print(s)
+s1=[66,67,68]
+s.extend(s1)
+print(s)
+
+'''
+
+s={1,3,2}
+s.add(4)
+print(s)
+#s.remove(3)
+#s.clear()
+
+a=int(input(''))
+if a>=90 and a<=100:
+   print(a='A')
+else:a<60
+   print (a='E')
+
+
+
+
+
+
+
+
+
+
+
+
+
